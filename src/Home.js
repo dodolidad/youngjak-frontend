d73705d9442ca6bc991c87bd9149187dd8893115
dodolidad <dodolidad@gmail.com>
2022-10-26 23:22:34 +0900

    text

--- src/Home.js
+++ src/Home.js
@@ -16,9 +16,13 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Accordion from '@mui/material/Accordion';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import TextField from '@mui/material/TextField';
 
 function Home(props, ref) {
   const navigate = useNavigate();
+  const [textKor, setTextKor] = React.useState('안녕하세요.');
+  const [textEn, setTextEn] = React.useState('Hello');
 
   return (
     <React.Fragment>
@@ -37,42 +41,35 @@ function Home(props, ref) {
         <Typography variant="h3" gutterBottom></Typography>
       </Box>
       <Box textAlign="right">
-        <Button color="success" variant="contained" endIcon={<CampaignIcon />}>
+        <Button onClick={() => props.play(textEn, 'en', 'female')} color="success" variant="contained" endIcon={<CampaignIcon />}>
           재생하기
         </Button>
       </Box>
+      
       <Typography variant="h6" gutterBottom></Typography>
-      <Accordion defaultExpanded={true}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Accordion 1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Accordion 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+
+      <TextField
+        label="한국어"
+        fullWidth
+        multiline
+        minRows="2"
+        defaultValue={textKor}
+        onChange={(e) => console.log(setTextKor(e.target.value))}
+      />
+
+      <Typography variant="h4" gutterBottom></Typography>
+
+      <TextField
+          label="영어"
+          fullWidth
+          multiline
+          minRows="2"
+          defaultValue={textEn}
+          onChange={(e) => console.log(setTextEn(e.target.value))}
+      />
+
       <Typography variant="h5" gutterBottom></Typography>
+
       <Box textAlign="center">
         <Button onClick={() => navigate('join')} size="large" color="success" variant="contained" endIcon={<TouchAppOutlinedIcon />}>
           회원가입
@@ -80,150 +77,6 @@ function Home(props, ref) {
      </Box>
     </React.Fragment>
   )
-
-  // const navigate = useNavigate();
-  // const accordionRef = React.useRef();
-
-  // const play = () => {
-  //   // const countdown = setInterval(() => {
-  //   //   TtsPlayerRef.current.play();
-  //   // }, 2000);
-  //   // TtsPlayerRef.current.play();
-  // } 
-
-  // const [expanded, setExpanded] = React.useState('panel1');
-
-  // const handleChange = (panel) => (event, newExpanded) => {
-  //   setExpanded(newExpanded ? panel : false);
-  // };
-
-  // return (
-  //   <React.Fragment>
-  //     <Typography variant="h4" gutterBottom></Typography>
-  //     <Box textAlign="center" sx={{ width: '100%' }}>
-  //       <Typography variant="h4" gutterBottom>  
-  //         Welcome!
-  //       </Typography>
-  //     </Box>
-  //     <Box sx={{ width: '100%' }}>
-  //       <Typography variant="h5" gutterBottom>
-  //         youngjak.com에서 영어회화 실력을 향상시키세요.
-  //       </Typography>
-  //       <li><strong>랜덤 재생:</strong> 내가 저장한 문장 또는 제안되는 문장을 자동 재생해보세요.</li>
-  //       <Typography variant="h3" gutterBottom></Typography>
-  //     </Box>
-
-  //     <Box textAlign="right">
-  //       <Button onClick={play} color="success" variant="contained" endIcon={<CampaignIcon />}>
-  //         재생하기
-  //       </Button>
-  //     </Box>
-
-  //     <Typography variant="h6" gutterBottom></Typography> 
-
-  //     {/* <Accordion ref={accordionRef} textPanel1='안녕 내이름은 탐이야.' textPanel2='I have to work at least 10 years' /> */}
-
-
-
-  //     <Typography variant="h3" gutterBottom></Typography>
-
-  //     <Box textAlign="center">
-  //       <Button onClick={() => navigate('join')} size="large" color="success" variant="contained" endIcon={<TouchAppOutlinedIcon />}>
-  //         회원가입
-  //       </Button>
-  //     </Box>
-  //   </React.Fragment>
-  // );
-
-
-
-  // const navigate = useNavigate();
-  // const accordionRef = React.useRef();
-  // const CommAlertRef = React.useRef();
-  // const TtsPlayerRef = React.useRef();
-
-  // // const requestAudioFile = async () => {
-  // //   console.log(accordionRef.current.textPanel1);
-  // //   const response = await axios.get(process.env.REACT_APP_API_URL + '/tts', {
-  // //     params: {
-  // //     text: accordionRef.current.textPanel2,
-  // //     lang: 'en-US',
-  // //     voice: 'male',
-  // //     },
-  // //     responseType : 'arraybuffer'
-  // //   })
-  // //   .then(async(res) => {
-  // //     const audioContext = new AudioContext(window.AudioContext || window.webkitAudioContext);
-
-  // //     const audioBuffer = await audioContext.decodeAudioData(res.data);
-
-  // //     const source = audioContext.createBufferSource();
-  // //     source.buffer = audioBuffer;
-  // //     source.connect(audioContext.destination);
-  // //     source.start();
-
-  // //     source.onended = (event) => {
-  // //       console.log('Video stopped either because 1) it was over, ' +
-  // //           'or 2) no further data is available.');
-  // //     };
-  // //   })
-  // //   .catch((err) => {
-  // //     commAlertType.current ='Error'
-  // //     commAlertMsg.current = '데이터 처리 실패. 다시 시도하세요.';
-  // //     CommAlertRef.current.handleClickOpen();
-  // //   });
-  // // }
-
-  // const openCommAlert = (type, msg) => {
-  //   commAlertType.current = type;
-  //   commAlertMsg.current = msg;
-  //   CommAlertRef.current.handleClickOpen();
-  // }
-
-  // const play = () => {
-  //   // const countdown = setInterval(() => {
-  //   //   TtsPlayerRef.current.play();
-  //   // }, 2000);
-  //   //TtsPlayerRef.current.play();
-  // } 
-
-  // return (
-  //   <>
-  //   <Typography variant="h4" gutterBottom></Typography>
-  //   <Box textAlign="center" sx={{ width: '100%' }}>
-  //     <Typography variant="h4" gutterBottom>  
-  //       Welcome!
-  //     </Typography>
-  //   </Box>
-  //   <Box sx={{ width: '100%' }}>
-  //     <Typography variant="h5" gutterBottom>
-  //       youngjak.com에서 영어회화 실력을 향상시키세요.
-  //     </Typography>
-  //     <li><strong>랜덤 재생:</strong> 내가 저장한 문장 또는 제안되는 문장을 자동 재생해보세요.</li>
-  //     <Typography variant="h3" gutterBottom></Typography>
-  //   </Box>
-
-  //   <Box textAlign="right">
-  //     <Button onClick={play} color="success" variant="contained" endIcon={<CampaignIcon />}>
-  //       재생하기
-  //     </Button>
-  //   </Box>
-
-  //   <Typography variant="h6" gutterBottom></Typography> 
-
-  //   <Accordion ref={accordionRef} textPanel1='안녕 내이름은 탐이야.' textPanel2='I have to work at least 10 years' />
-
-  //   <Typography variant="h3" gutterBottom></Typography>
-
-  //   <Box textAlign="center">
-  //     <Button onClick={() => navigate('join')} size="large" color="success" variant="contained" endIcon={<TouchAppOutlinedIcon />}>
-  //       회원가입
-  //     </Button>
-  //   </Box>
-  //   <TtsPlayer ref={TtsPlayerRef} openCommAlert={openCommAlert} text='hello' lang='en-US' voice='FEMALE'/>
-  //   <CommAlert ref={CommAlertRef} type={commAlertType} msg={commAlertMsg} />
-  //   </>
-  // );
 }
 
 export default React.forwardRef(Home);
